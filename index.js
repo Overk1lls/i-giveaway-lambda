874e5fc7ae6b1bde6678d92250c0e970cf93e0ff
Yury <ov3rfordream@gmail.com>
2021-12-02 16:28:24 +0200

    exist in all files

--- index.js
+++ index.js
@@ -10,7 +10,21 @@ const uniqueValues = () => {
     return result;
 };
 
-console.time('Unique Values');
-console.log(uniqueValues().size);
-console.timeEnd('Unique Values');
+const existInAllFiles = () => {
+    let result = fs.readFileSync('./words/out0.txt').toString().split('\n');
+    
+    for (let i = 1; i < 20; i++) {
+        const arrayData = fs.readFileSync(`./words/out${i}.txt`).toString().split('\n');
+        result = result.filter(element => arrayData.includes(element));
+    }
+    return result;
+};
 
+console.time('Unique values');
+console.log(uniqueValues().size);
+console.timeEnd('Unique values');
+
+console.time('Exist in all files');
+const existsInAll = existInAllFiles();
+console.log(existsInAll.length);
+console.timeEnd('Exist in all files');
