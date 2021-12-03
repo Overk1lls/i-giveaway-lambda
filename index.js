6f656f32c96668923c4dce84099b4cc6295248e1
Yury <ov3rfordream@gmail.com>
2021-12-03 03:00:05 +0200

    exist in at least 10

--- index.js
+++ index.js
@@ -12,12 +12,27 @@ const uniqueValues = () => {
 
 const existInAllFiles = () => {
     let result = fs.readFileSync('./words/out0.txt').toString().split('\n');
-    
+
     for (let i = 1; i < 20; i++) {
         const arrayData = fs.readFileSync(`./words/out${i}.txt`).toString().split('\n');
         result = result.filter(element => arrayData.includes(element));
     }
     return result;
+};
+
+const existInAtLeastTen = () => {
+    const obj = {};
+
+    for (let i = 0; i < 20; i++) {
+        const arrayData = fs.readFileSync(`./words/out${i}.txt`).toString().split('\n');
+        const set = new Set(arrayData);
+        
+        set.forEach(element => {
+            if (element in obj) obj[element]++;
+            else obj[element] = 1;
+        });
+    }
+    return Object.keys(obj).filter((key, value) => value >= 10);
 };
 
 console.time('Unique values');
@@ -28,3 +43,8 @@ console.time('Exist in all files');
 const existsInAll = existInAllFiles();
 console.log(existsInAll.length);
 console.timeEnd('Exist in all files');
+
+console.time('Exist in at least ten files');
+const existInTen = existInAtLeastTen();
+console.log(existInTen.length);
+console.timeEnd('Exist in at least ten files');
